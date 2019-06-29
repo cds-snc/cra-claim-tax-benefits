07b97ab4cf11d715c67ef4e5a0702227586c44d6
Paul Craig <paul_craig_16@hotmail.com>
2019-06-29 19:11:01 -0400

    Add template for start page
    Added the content and a basic semantic structure that makes sense
    for the page we're looking to build.
    
    Also removed some left-around CSS rules we don't actually care about.

--- routes/start/start.controller.js
+++ routes/start/start.controller.js
@@ -5,5 +5,5 @@ const express = require('express'),
 module.exports = function(app) {
   // redirect from "/" → "/start"
   app.get('/', (req, res, next) => res.redirect('/start'))
-  app.get('/start', (req, res, next) => res.render('index'))
+  app.get('/start', (req, res, next) => res.render('start/index'))
 }

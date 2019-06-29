f7204be9468de65579cc15eb05f3322786b3ba6c
Paul Craig <paul_craig_16@hotmail.com>
2019-06-29 19:52:37 -0400

    Remove 'production' check for helmet
    Headers and compression are pretty small overhead, so let's just
    turn them on all the time.

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ const express = require('express'),
 // initialize application.
 var app = express()
 
+// view engine setup
+app.set('views', path.join(__dirname, './views'))
+app.set('view engine', 'pug')
+
 // general app configuration.
 app.use(morgan('combined', { stream: winston.stream }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser(process.env.app_session_secret))
-
-// view engine setup
-app.set('views', path.join(__dirname, './views'))
-app.set('view engine', 'pug')
 
 /* in a real-life use case, we would precompile the CSS */
 app.use(
@@ -39,18 +39,16 @@ app.use(
 // public assets go here (css, js, etc)
 app.use(express.static(path.join(__dirname, 'public')))
 
-if (app.get('env') === 'production') {
-  // dnsPrefetchControl controls browser DNS prefetching
-  // frameguard to prevent clickjacking
-  // hidePoweredBy to remove the X-Powered-By header
-  // hsts for HTTP Strict Transport Security
-  // ieNoOpen sets X-Download-Options for IE8+
-  // noSniff to keep clients from sniffing the MIME type
-  // xssFilter adds some small XSS protections
-  app.use(helmet())
-  // gzip response body compression.
-  app.use(compression())
-}
+// dnsPrefetchControl controls browser DNS prefetching
+// frameguard to prevent clickjacking
+// hidePoweredBy to remove the X-Powered-By header
+// hsts for HTTP Strict Transport Security
+// ieNoOpen sets X-Download-Options for IE8+
+// noSniff to keep clients from sniffing the MIME type
+// xssFilter adds some small XSS protections
+app.use(helmet())
+// gzip response body compression.
+app.use(compression())
 
 // configure routes
 require('./routes/start/start.controller')(app)

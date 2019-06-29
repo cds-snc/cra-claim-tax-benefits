05c11b1abd78216ca1a730f770bb6ec58a78ca49
Paul Craig <paul_craig_16@hotmail.com>
2019-06-29 15:54:11 -0400

    Remove session routes
    Remove the session routes for now so that the app setup is easier.
    
    Brought the view configuration into the main app configuration
    because we're building a website and we need the same config for
    all of our controllers going forward.

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 // import environment variables.
-require("dotenv").config();
+require('dotenv').config()
 
 // import node modules.
-const express = require("express"),
-  cookieParser = require("cookie-parser"),
-  compression = require("compression"),
-  helmet = require("helmet"),
-  morgan = require("morgan"),
-  winston = require("./config/winston.config"),
-  sassMiddleware = require("node-sass-middleware"),
-  path = require("path");
+const express = require('express'),
+  cookieParser = require('cookie-parser'),
+  compression = require('compression'),
+  helmet = require('helmet'),
+  morgan = require('morgan'),
+  winston = require('./config/winston.config'),
+  sassMiddleware = require('node-sass-middleware'),
+  path = require('path')
 
 // initialize application.
-var app = express();
+var app = express()
 
 // general app configuration.
-app.use(morgan("combined", { stream: winston.stream }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.app_session_secret));
+app.use(morgan('combined', { stream: winston.stream }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser(process.env.app_session_secret))
+
+// view engine setup
+app.set('views', path.join(__dirname, './views'))
+app.set('view engine', 'pug')
 
 /* in a real-life use case, we would precompile the CSS */
 app.use(
@@ -27,12 +31,14 @@ app.use(
     dest: path.join(__dirname),
     debug: true,
     indentedSyntax: false,
-    sourceMap: true
-  })
-);
-app.use(express.static(path.join(__dirname, "public")));
+    sourceMap: true,
+  }),
+)
 
-if (app.get("env") !== "development") {
+// public assests go here (css, js, etc)
+app.use(express.static(path.join(__dirname, 'public')))
+
+if (app.get('env') !== 'development') {
   // dnsPrefetchControl controls browser DNS prefetching
   // frameguard to prevent clickjacking
   // hidePoweredBy to remove the X-Powered-By header
@@ -40,30 +46,25 @@ if (app.get("env") !== "development") {
   // ieNoOpen sets X-Download-Options for IE8+
   // noSniff to keep clients from sniffing the MIME type
   // xssFilter adds some small XSS protections
-  app.use(helmet());
+  app.use(helmet())
   // gzip response body compression.
-  app.use(compression());
+  app.use(compression())
 }
 
-// configure routes  ... basic session api strategy w/h redis.
-require("../expressbase/routes/session/session.controller")(app);
-
 // configure routes  ... basic ui strategy w/h pug.
-require("../expressbase/routes/ui/ui.controller")(app);
+require('./routes/ui/ui.controller')(app)
 
 // handle global errors.
 app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  winston.debug(`Service error: ${err}`);
+  winston.debug(`Service error: ${err}`)
   winston.error(
-    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
-      req.method
-    } - ${req.ip}`
-  );
+    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`,
+  )
 
-  res.status(err.status || 500).json({ message: "Internal service error." });
-});
+  res.status(err.status || 500).json({ message: 'Internal service error.' })
+})
 
-module.exports = app;
+module.exports = app
--- routes/ui/ui.controller.js
+++ routes/ui/ui.controller.js
@@ -1,29 +1,18 @@
-const 
-  express = require('express'),
+const express = require('express'),
   logger = require('../../config/winston.config'),
-  path = require('path');
+  path = require('path')
 
-module.exports = function (app) {
-
-  // view engine setup
-  app.set('views', path.join(__dirname, '../../views'));
-  app.set('view engine', 'pug');
-
-  // public assests go here (css, js, etc)
-  app.use('/public', express.static(path.join(__dirname, '../../public')));
-
+module.exports = function(app) {
   // ui routes.
-  app.get('/expressbase/ui/', get);
-};
-
-function get (req, res, next) {
-  try {
-    logger.debug('[UI Route] get()');
-    res.render('index');
-  } 
-  catch (err) {
-    logger.error(`[UI Service] get() ${err}`);
-    res.render('error');
-  }
+  app.get('/expressbase/ui/', get)
 }
 
+function get(req, res, next) {
+  try {
+    logger.debug('[UI Route] get()')
+    res.render('index')
+  } catch (err) {
+    logger.error(`[UI Service] get() ${err}`)
+    res.render('error')
+  }
+}

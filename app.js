6670eff3dbc6472c23214d78868fdc9240985bbc
Paul Craig <paul_craig_16@hotmail.com>
2019-06-29 20:12:35 -0400

    Don't configure logger when running tests
    Adds lots of noise we don't need.
    
    Also reformatted the file on save (and changed a variable name).

--- app.js
+++ app.js
@@ -18,13 +18,15 @@ var app = express()
 app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'pug')
 
+// if NODE_ENV does not equal 'test', add a request logger
+process.env.NODE_ENV !== 'test' && app.use(morgan('combined', { stream: winston.stream }))
+
 // general app configuration.
-app.use(morgan('combined', { stream: winston.stream }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser(process.env.app_session_secret))
 
-/* in a real-life use case, we would precompile the CSS */
+// in a real-life use case, we would precompile the CSS
 app.use(
   sassMiddleware({
     src: path.join(__dirname, 'public'),

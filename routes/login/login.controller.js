b9f13907f2b6b7e805d22f4a9b52126470bac7dc
Paul Craig <paul_craig_16@hotmail.com>
2019-06-30 12:43:33 -0400

    Save submitted value in user session

--- routes/login/login.controller.js
+++ routes/login/login.controller.js
@@ -1,5 +1,15 @@
 module.exports = function(app) {
   // redirect from "/login" → "/login/accessCode"
   app.get('/login', (req, res) => res.redirect('/login/code'))
-  app.get('/login/code', (req, res) => res.render('login/code', { title: 'Enter access code' }))
+  app.get('/login/code', (req, res) =>
+    res.render('login/code', { title: 'Enter access code', data: req.session || {} }),
+  )
+  app.post('/login/code', postCode)
+}
+
+const postCode = (req, res) => {
+  let accessCode = req.body.code || null
+  req.session = accessCode ? { code: accessCode } : null
+
+  res.render('login/code', { title: 'Enter access code', data: req.session || {} })
 }

4039c1f4f5d76d0352b77a65c03a9e0a395cda7d
Paul Craig <paul_craig_16@hotmail.com>
2019-06-29 21:37:10 -0400

    Add test for English vs French responses
    One of the features of i18n is that it's supposed to look at the
    browser 'Accept-Language' header to decide what to do.
    Added a test to confirm this behaviour.

--- routes/start/start.spec.js
+++ routes/start/start.spec.js
@@ -3,21 +3,38 @@ const cheerio = require('cheerio')
 const app = require('../../app.js')
 
 describe('Test server responses', () => {
-  test('it should redirect to /start for the root path', async () => {
+  test('it redirects to /start for the root path', async () => {
     const response = await request(app).get('/')
     expect(response.statusCode).toBe(302)
     expect(response.headers.location).toEqual('/start')
   })
 
-  test('it should show the h1 for the /start path', async () => {
+  test('it returns a 200 response for the /start path', async () => {
     const response = await request(app).get('/start')
     expect(response.statusCode).toBe(200)
-
-    const $ = cheerio.load(response.text)
-    expect($('h1').text()).toEqual('Claim tax benefits')
   })
 
-  test('it should return security-focused headers in reponses', async () => {
+  describe('it renders the h1 text for the /start path', () => {
+    test('in English', async () => {
+      const response = await request(app)
+        .get('/start')
+        .set('Accept-Language', 'en')
+
+      const $ = cheerio.load(response.text)
+      expect($('h1').text()).toEqual('Claim tax benefits')
+    })
+
+    test('in French', async () => {
+      const response = await request(app)
+        .get('/start')
+        .set('Accept-Language', 'fr-CA, fr;q=0.9, en;q=0.8')
+
+      const $ = cheerio.load(response.text)
+      expect($('h1').text()).toEqual('Réclamer des avantages fiscaux')
+    })
+  })
+
+  test('it returns security-focused headers in reponses', async () => {
     const response = await request(app).get('/start')
 
     /*

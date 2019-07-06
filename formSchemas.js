9fb479cf9f907add1883c4a3754050deef2caef7
Paul Craig <paul_craig_16@hotmail.com>
2019-07-05 21:45:02 -0400

    Validate login code against the current user login code
    Also changed the error message to be a key value so that we can
    keep the translations for the error messages in the same place.

--- formSchemas.js
+++ formSchemas.js
@@ -1,15 +1,21 @@
-const loginSchema = {
-    code: {
-      isLength: {
-        errorMessage: 'Must be 8 characters',
-        options: { min: 8, max: 8 }
-      },
-      isAlphanumeric: {
-          errorMessage: 'Code can only contain letters and numbers'
-      }
-    },
-  }
+const API = require('./api')
 
-  module.exports = {
-    loginSchema
-  }
+const loginSchema = {
+  code: {
+    isLength: {
+      errorMessage: 'Must be 8 characters',
+      options: { min: 8, max: 8 },
+    },
+    isAlphanumeric: {
+      errorMessage: 'Code can only contain letters and numbers',
+    },
+    isIn: {
+      options: [API.getMatches()],
+      errorMessage: 'errors.login.code',
+    },
+  },
+}
+
+module.exports = {
+  loginSchema,
+}

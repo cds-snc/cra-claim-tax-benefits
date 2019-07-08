5ef1d300dd0fabd539eb0525bc306112dfff8143
Kate Drwecka <katedrwecka@gmail.com>
2019-07-08 16:29:31 -0400

    base pages for confirm and edit marital status some of the basic styling for the radiobuttons is there need to add working with API and tests

--- routes/personal/personal.controller.js
+++ routes/personal/personal.controller.js
@@ -1,3 +1,5 @@
 module.exports = function(app) {
   app.get('/personal/address', (req, res) => res.render('personal/address'))
+  app.get('/personal/maritalStatus', (req, res) => res.render('personal/maritalStatus'))
+  app.get('/personal/maritalStatus/edit', (req, res) => res.render('personal/maritalStatus-edit'))
 }

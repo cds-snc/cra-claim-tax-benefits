0c9be7baab13509cfc0cb7c1ee3a894a88479cfc
Stéphane Boisvert <stephane.boisvert@cds-snc.ca>
2019-07-11 12:35:01 -0400

    Change to use global redirec validation

--- routes/deductions/deductions.controller.js
+++ routes/deductions/deductions.controller.js
@@ -1,5 +1,5 @@
 const { validationResult } = require('express-validator')
-const { errorArray2ErrorObject } = require('./../../utils.js')
+const { errorArray2ErrorObject, validateRedirect } = require('./../../utils.js')
 //const { loginSchema, sinSchema } = require('./../../formSchemas.js')
 //const API = require('../../api')
 
@@ -8,22 +8,11 @@ module.exports = function (app) {
   app.get('/deductions', (req, res) => res.redirect('/login/code'))
 
   app.get('/deductions/rrsp', (req, res) => res.render('deductions/rrsp', { data: req.session || {} }))
-  app.post('/deductions/rrsp', postRRSP)
+  app.post('/deductions/rrsp', validateRedirect, postRRSP)
 
-}
-
-//POST functions that handle setting the login data in the session and handle redirecting to the next page or sending an error to the client.
-//Note that this is not the only error validation, see routes defined above.
-const validateRedirect = req => {
-  let redirect = req.body.redirect || null
-  if (!redirect) {
-    throw new Error(`[POST ${req.path}] 'redirect' parameter missing`)
-  }
-  return redirect
 }
 
 const postRRSP = (req, res) => {
-  const redirect = validateRedirect(req, res)
 
   const errors = validationResult(req)
 
@@ -45,6 +34,8 @@ const postRRSP = (req, res) => {
 
   //No errors, but didn't process. Unsure if this code branch is reachable but it's a good safety.
   res.status(422).render('deductions/rrsp', { data: req.session || {} })
+  //Success, we can redirect to the next page
+  return res.redirect(req.body.redirect)
 
 }
 

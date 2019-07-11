062cd3f70f07742efbbbd63c7a266e5ca8b60bac
Kate Drwecka <katedrwecka@gmail.com>
2019-07-11 10:21:32 -0400

    comment about formRadios

--- formRadios.js
+++ formRadios.js
@@ -1,3 +1,4 @@
+// haven't used this elsewhere yet, but the idea of keeping it here is so we can pull these values in wherever we're using them, and have one source of truth for these values (instead of updating copy in multiple files)
 const maritalStatusValues = {
     en: ['Single','Married','Separated', 'Divorced', 'Widowed']
 }

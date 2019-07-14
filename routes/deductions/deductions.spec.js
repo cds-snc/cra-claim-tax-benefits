89252c52fb13091eb02c77ca093488e0360b6461
Paul Craig <paul_craig_16@hotmail.com>
2019-07-14 00:51:34 -0400

    Add tests for validating against differenct currencies
    There appears to be no way within the currency validators to ban
    people from putting in "0.00", but I can live with that.

--- routes/deductions/deductions.spec.js
+++ routes/deductions/deductions.spec.js
@@ -42,13 +42,43 @@ describe('Test /deductions responses', () => {
     })
   })
 
-  test('it returns a 200 response for /deductions/rrsp/amount', async () => {
-    const response = await request(app).get('/deductions/rrsp')
-    expect(response.statusCode).toBe(200)
-  })
+  describe('Test /deductions/rrsp/amount responses', () => {
+    test('it returns a 200 response for /deductions/rrsp/amount', async () => {
+      const response = await request(app).get('/deductions/rrsp')
+      expect(response.statusCode).toBe(200)
+    })
 
-  test('it returns a 500 response if no redirect is provided', async () => {
-    const response = await request(app).post('/deductions/rrsp/amount')
-    expect(response.statusCode).toBe(500)
+    test('it returns a 500 response if no redirect is provided', async () => {
+      const response = await request(app).post('/deductions/rrsp/amount')
+      expect(response.statusCode).toBe(500)
+    })
+
+    test('it returns a 422 response for no posted value', async () => {
+      const response = await request(app)
+        .post('/deductions/rrsp/amount')
+        .send({ redirect: '/' })
+      expect(response.statusCode).toBe(422)
+    })
+
+    const badRRSPAmounts = ['', null, 'dinosaur', '10.0', '10.000', '-10', '.1']
+    badRRSPAmounts.map(rrspAmount => {
+      test(`it returns a 422 for a bad posted value: "${rrspAmount}"`, async () => {
+        const response = await request(app)
+          .post('/deductions/rrsp/amount')
+          .send({ rrspAmount, redirect: '/' })
+        expect(response.statusCode).toBe(422)
+      })
+    })
+
+    const goodRRSPAmounts = ['0', '10', '10.00', '.10']
+    goodRRSPAmounts.map(rrspAmount => {
+      test(`it returns a 302 for a good posted value: "${rrspAmount}"`, async () => {
+        const response = await request(app)
+          .post('/deductions/rrsp/amount')
+          .send({ rrspAmount, redirect: '/' })
+        expect(response.statusCode).toBe(302)
+        expect(response.headers.location).toEqual('/')
+      })
+    })
   })
 })

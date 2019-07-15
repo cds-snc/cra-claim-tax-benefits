3de2331e939d69db3012c5dcf3a16a7f119d3d9e
Paul Craig <paul_craig_16@hotmail.com>
2019-07-15 09:20:01 -0400

    Format formSchemas.js
    Also removed a couple of things
    
    - one unused param on line 18
    - an unnecessary backslash on line 102
    
    (because my editor told me, not because I am hawk-eyed)

--- formSchemas.js
+++ formSchemas.js
@@ -6,29 +6,29 @@ const API = require('./api')
  * but still maintain individual error messages
  */
 const validationArray = validators => {
-	let errors = [];
+  let errors = []
 
-	return {
-		custom: {
-			errorMessage: () => {
-				return errors.length ? errors[0] : 'value is invalid';
-			},
-			options: (value, { req }, opts) => {
-				errors = [];
-				const results = validators.map((validator, index) => {
-					const result = validator.validate(value, req, opts);
-					// If validation failed set current Error
-					if (result === false) {
-						const errorMessage = validator.errorMessage || `${value} is invalid`;
-						errors.push(errorMessage);
-					}
-					return result;
-				});
-				return results.every(value => value === true);
-			}
-		}
-	};
-};
+  return {
+    custom: {
+      errorMessage: () => {
+        return errors.length ? errors[0] : 'value is invalid'
+      },
+      options: (value, { req }, opts) => {
+        errors = []
+        const results = validators.map(validator => {
+          const result = validator.validate(value, req, opts)
+          // If validation failed set current Error
+          if (result === false) {
+            const errorMessage = validator.errorMessage || `${value} is invalid`
+            errors.push(errorMessage)
+          }
+          return result
+        })
+        return results.every(value => value === true)
+      },
+    },
+  }
+}
 
 const loginSchema = {
   code: {
@@ -80,52 +80,55 @@ const sinSchema = {
   },
 }
 
-const lastDayInMonth = (year,month) => {
-  return  new Date(year, month +1, 0).getDate();
+const lastDayInMonth = (year, month) => {
+  return new Date(year, month + 1, 0).getDate()
 }
 
 const validBirthDateLengths = {
   errorMessage: 'errors.login.dateOfBirth.format',
   validate: value => {
     const birthDateSplit = value.split('/')
-    return birthDateSplit[0].length === 4 && birthDateSplit[1].length === 2 && birthDateSplit[2].length === 2
-  }
+    return (
+      birthDateSplit[0].length === 4 &&
+      birthDateSplit[1].length === 2 &&
+      birthDateSplit[2].length === 2
+    )
+  },
 }
 
 const validBirthDateChars = {
   errorMessage: 'errors.login.dateOfBirth.characters',
-  validate : value => {
-    const numAndSlash = new RegExp(/^[0-9\/]*$/)
+  validate: value => {
+    const numAndSlash = new RegExp(/^[0-9/]*$/)
     return numAndSlash.test(value)
-  }
+  },
 }
 
 const validMonth = {
   errorMessage: 'errors.login.dateOfBirth.validMonth',
-  validate : value => {
+  validate: value => {
     //month is not less than 1 or greater than 12
-    const month = parseInt(value.split('/')[1], 10);
-    return month >= 1 && month <= 12 
-  }
+    const month = parseInt(value.split('/')[1], 10)
+    return month >= 1 && month <= 12
+  },
 }
 
 const validDay = {
   errorMessage: 'errors.login.dateOfBirth.validDay',
-  validate : value => {
+  validate: value => {
     //day is within the acceptable range
     const dateSplit = value.split('/')
     //subtract one because Date for months starts at a 0 index for Jan
     const month = parseInt(dateSplit[1]) - 1
     const day = parseInt(dateSplit[2], 10)
     const lastDay = lastDayInMonth(dateSplit[0], month)
-    return day >= 1 && day <= lastDay 
-  
-  }
+    return day >= 1 && day <= lastDay
+  },
 }
 
 const validYear = {
   errorMessage: 'errors.login.dateOfBirth.validYear',
-  validate : value => {
+  validate: value => {
     //year is within the acceptable range. Person not older than 200, not younger than 1
     //listen, if you've lived to 201, why are you even bothering with taxes?
     const dateEntered = new Date(value)
@@ -136,10 +139,10 @@ const validYear = {
 
     const lastYear = new Date(year - 1, month, day)
 
-    const longAgo = new Date(year -200, month, day)
+    const longAgo = new Date(year - 200, month, day)
 
     return dateEntered <= lastYear && dateEntered >= longAgo
-  }
+  },
 }
 
 const isMatchingDoB = {
@@ -151,7 +154,7 @@ const isMatchingDoB = {
     }
 
     return value === req.session.personal.dob
-  }
+  },
 }
 
 const birthSchema = {
@@ -176,8 +179,7 @@ const birthSchema = {
       //length with slashes
       options: { min: 10, max: 10 },
     },
-    
-  }
+  },
 }
 
 //TODO: We'll want to store this array of marital options somewhere. This is temporary. I'll also want to use that later to create the radio buttons dynamically, to avoid having to update multiple files

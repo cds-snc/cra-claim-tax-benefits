2ea13412ffd905bf01acde6b33dd02ddc0a2048c
Kate Drwecka <katedrwecka@gmail.com>
2019-07-16 11:04:16 -0400

    fix spaces

--- routes/offramp/offramp.controller.js
+++ routes/offramp/offramp.controller.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-    app.get('/offramp', (req, res) =>
-      res.render('offramp/offramp', { data: req.session }),
-    )
+  app.get('/offramp', (req, res) =>
+    res.render('offramp/offramp', { data: req.session }),
+  )
 }

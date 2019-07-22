cae0fccf0670871ecbeeb10e7c96ae0e4f48317f
Paul Craig <paul_craig_16@hotmail.com>
2019-07-21 20:09:19 -0400

    Add /commencer route so that we can load the site in french
    We haven't really solved the problem that all our URLs are in
    English, but that doesn't mean that we have to make people
    type a query parameter just to see the site in french from the jump.
    
    Added a new route that redirects to the start page in French.
    
    Pretty simple, works good enough for now.

--- routes/start/start.controller.js
+++ routes/start/start.controller.js
@@ -2,4 +2,9 @@ module.exports = function(app) {
   // redirect from "/" → "/start"
   app.get('/', (req, res) => res.redirect('/start'))
   app.get('/start', (req, res) => res.render('start/index'))
+
+  // loads the site in french from the get-go
+  app.get('/commencer', (req, res) => {
+    res.redirect('/start?lang=fr')
+  })
 }

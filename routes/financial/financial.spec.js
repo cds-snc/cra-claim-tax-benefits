e063933243749e7af7335ae9f11b1235747778a1
Kate Drwecka <katedrwecka@gmail.com>
2019-07-22 10:40:29 -0400

    add tests for confirm income

--- routes/financial/financial.spec.js
+++ routes/financial/financial.spec.js
@@ -6,4 +6,27 @@ describe('Test /financial responses', () => {
     const response = await request(app).get('/financial/income')
     expect(response.statusCode).toBe(200)
   })
+
+  test('it returns a 422 with no option selected', async () => {
+    const response = await request(app)
+      .post('/financial/income')
+      .send({ redirect: '/financial/income' })
+    expect(response.statusCode).toBe(422)
+  })
+
+  test('it returns a 302 and redirects to offramp when NO is selected', async () => {
+    const response = await request(app)
+      .post('/financial/income')
+      .send({ confirmIncome: 'No', redirect: '/offramp' })
+      expect(response.statusCode).toBe(302)
+      expect(response.headers.location).toEqual('/offramp')
+  })
+
+  test('it returns a 302 and redirects to the same page when YES is selected', async () => {
+    const response = await request(app)
+      .post('/financial/income')
+      .send({ confirmIncome: 'Yes', redirect: '/financial/income' })
+      expect(response.statusCode).toBe(302)
+      expect(response.headers.location).toEqual('/financial/income')
+  })
 })

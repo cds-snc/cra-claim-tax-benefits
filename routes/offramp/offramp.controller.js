c05080ab75fe03575d43758281a05dd124c462a0
nmakuch <makuch.nick@gmail.com>
2019-07-25 20:33:49 -0400

    offramp-financial page created

--- routes/offramp/offramp.controller.js
+++ routes/offramp/offramp.controller.js
@@ -10,4 +10,8 @@ module.exports = function(app) {
   app.get('/offramp-residence', (req, res) =>
   res.render('offramp/offramp-residence', { data: req.session }),
   )
+
+  app.get('/offramp-financial', (req, res) =>
+  res.render('offramp/offramp-financial', { data: req.session }),
+  )
 }

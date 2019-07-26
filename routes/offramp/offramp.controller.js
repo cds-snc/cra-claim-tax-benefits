f1ba370b89019059755ee3791230dbc0d71d1e31
nmakuch <makuch.nick@gmail.com>
2019-07-25 20:01:02 -0400

    offramp-name page

--- routes/offramp/offramp.controller.js
+++ routes/offramp/offramp.controller.js
@@ -2,4 +2,8 @@ module.exports = function(app) {
   app.get('/offramp', (req, res) =>
     res.render('offramp/offramp', { data: req.session }),
   )
+
+  app.get('/offramp-name', (req, res) =>
+  res.render('offramp/offramp-name', { data: req.session }),
+)
 }

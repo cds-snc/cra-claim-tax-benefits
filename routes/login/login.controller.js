55b0531e759eda0ff5f8194ddc6fd88819616170
System Administrator <root@Ayos-MacBook-Pro.local>
2019-08-02 11:51:18 -0400

    adding service api call

--- routes/login/login.controller.js
+++ routes/login/login.controller.js
@@ -2,6 +2,7 @@ const { validationResult, checkSchema } = require('express-validator')
 const { errorArray2ErrorObject, validateRedirect, checkErrors } = require('./../../utils')
 const { loginSchema, sinSchema, birthSchema, authSchema } = require('./../../formSchemas.js')
 const API = require('../../api')
+const request = require('request-promise')
 
 module.exports = function(app) {
   // redirect from "/login" → "/login/code"
@@ -36,7 +37,7 @@ module.exports = function(app) {
   app.post('/login/auth', checkSchema(authSchema), checkErrors('login/auth'), postAuth)
 }
 
-const postLoginCode = (req, res) => {
+const postLoginCode = async (req, res) => {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
@@ -49,7 +50,12 @@ const postLoginCode = (req, res) => {
     })
   }
 
-  let user = API.getUser(req.body.code || null)
+  let user 
+
+  if (process.env.CTBS_SERVICE_URL && req.body.code) 
+    user = await request({ method: 'GET', uri: `${process.env.CTBS_SERVICE_URL}/${req.body.code}`, json: true})
+  else
+    user = API.getUser(req.body.code || null)
 
   if (!user) {
     throw new Error(`[POST ${req.path}] user not found for access code "${req.body.code}"`)

fe62cba2a5fb263afa457593f09f692600d4cf1f
Kate Drwecka <katedrwecka@gmail.com>
2019-09-12 19:13:00 -0400

    remove doAuth from personal controller since no longer in use

--- routes/personal/personal.controller.js
+++ routes/personal/personal.controller.js
@@ -1,5 +1,5 @@
 const { checkSchema } = require('express-validator')
-const { doRedirect, renderWithData, checkErrors, doAuth } = require('./../../utils')
+const { doRedirect, renderWithData, checkErrors } = require('./../../utils')
 const {
   addressSchema,
   maritalStatusSchema,

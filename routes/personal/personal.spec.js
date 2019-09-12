5427e8fc967e4095e34df2f4986aedd4079e1e18
Kate Drwecka <katedrwecka@gmail.com>
2019-09-12 18:43:33 -0400

    Add Yes/No check to maritalStatus and address pgs Remove routes for those same edit pages, since we're no longer allowing editing that information update the schemas to yes/no, and the test files add post routes for maritalStatus and Address

--- routes/personal/personal.spec.js
+++ routes/personal/personal.spec.js
@@ -8,10 +8,8 @@ describe('Test /personal responses', () => {
     const urls = [
       '/personal/name',
       '/personal/maritalStatus',
-      '/personal/maritalStatus/edit',
       '/personal/residence',
       '/personal/address',
-      '/personal/address/edit',
     ]
 
     urls.map(url => {
@@ -46,39 +44,11 @@ describe('Test /personal responses', () => {
   })
 
   describe('Test /personal/maritalStatus responses', () => {
-    test('it has Married selected by default', async () => {
-      const response = await request(app).get('/personal/maritalStatus')
-      const $ = cheerio.load(response.text)
-      expect($('td div').text()).toEqual('Single')
-    })
-
-    test('it checks the stored marital status by default for /personal/maritalStatus/edit path', async () => {
-      const user = API.getUser('A5G98S4K1')
-      const response = await request(app).get('/personal/maritalStatus/edit', { data: user })
-      const $ = cheerio.load(response.text)
-      expect($('input[name=maritalStatus]:checked').val().toLowerCase).toEqual(
-        user.personal.maritalStatus.toLowerCase,
-      )
-    })
-
-    test('it returns a 422 with no marital status', async () => {
+    test('it redirects to the offramp page when selecting No', async () => {
       const response = await request(app)
-        .post('/personal/maritalStatus/edit')
-        .send({ redirect: '/personal/maritalStatus' })
-      expect(response.statusCode).toBe(422)
-    })
-
-    test('it returns a 422 with fake marital status', async () => {
-      const response = await request(app)
-        .post('/personal/maritalStatus/edit')
-        .send({ maritalStatus: 'cat lady', redirect: '/personal/maritalStatus' })
-      expect(response.statusCode).toBe(422)
-    })
-
-    test('it returns a 302 with valid marital status', async () => {
-      const response = await request(app)
-        .post('/personal/maritalStatus/edit')
-        .send({ redirect: '/personal/maritalStatus', maritalStatus: 'Widowed' })
+        .post('/personal/maritalStatus')
+        .send({ confirmMaritalStatus: 'No' })
+      expect(response.headers.location).toEqual('/offramp')
       expect(response.statusCode).toBe(302)
     })
   })
@@ -109,88 +79,11 @@ describe('Test /personal responses', () => {
   })
 
   describe('Test /personal/address responses', () => {
-    let goodRequest = {
-      line1: 'Awesome Avenue',
-      city: 'Awesawa',
-      postalCode: 'H3L1Y4',
-      province: 'Ontario',
-      redirect: '/personal/address',
-    }
-
-    const badRequests = [
-      {
-        label: 'no streetAddress or city or postalCode or province',
-        firstErrorId: '#line1',
-        send: {
-          ...goodRequest,
-          ...{ line1: '', city: '', postalCode: '', province: '' },
-        },
-      },
-      {
-        label: 'no streetAddress',
-        firstErrorId: '#line1',
-        send: {
-          ...goodRequest,
-          ...{ line1: '' },
-        },
-      },
-      {
-        label: 'no city',
-        firstErrorId: '#city',
-        send: {
-          ...goodRequest,
-          ...{ city: '' },
-        },
-      },
-      {
-        label: 'no postalCode',
-        firstErrorId: '#postalCode',
-        send: {
-          ...goodRequest,
-          ...{ postalCode: '' },
-        },
-      },
-      {
-        label: 'bad postalCode',
-        firstErrorId: '#postalCode',
-        send: {
-          ...goodRequest,
-          ...{ postalCode: '0h3 N03' },
-        },
-      },
-      {
-        label: 'no province',
-        firstErrorId: '#province',
-        send: {
-          ...goodRequest,
-          ...{ province: '' },
-        },
-      },
-      {
-        label: 'bad province',
-        firstErrorId: '#province',
-        send: {
-          ...goodRequest,
-          ...{ province: 'Aurora' },
-        },
-      },
-    ]
-
-    badRequests.map(badRequest => {
-      test(`it returns a 422 with: "${badRequest.label}"`, async () => {
-        const response = await request(app)
-          .post('/personal/address/edit')
-          .send(badRequest.send)
-        const $ = cheerio.load(response.text)
-        expect($(badRequest.firstErrorId).attr('autofocus')).toEqual('autofocus')
-        expect(response.statusCode).toBe(422)
-      })
-    })
-
-    test('it returns a 302 with valid address', async () => {
+    test('it redirects to the offramp page when selecting No', async () => {
       const response = await request(app)
-        .post('/personal/address/edit')
-        .send(goodRequest)
+        .post('/personal/address')
+        .send({ confirmAddress: 'No' })
+      expect(response.headers.location).toEqual('/offramp')
       expect(response.statusCode).toBe(302)
     })
   })

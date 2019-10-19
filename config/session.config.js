73583abaa2d863d1873cb21dbe03ec186c5dac58
Paul Craig <paul_craig_16@hotmail.com>
2019-10-19 13:11:59 -0400

    Set resave and saveUninitialised to false
    Express session complains if these aren't set. Looked up the options
    and it seemed like we wanted false for both.
    
    Also used the same cookie settings that we had been using for cookie-session.
    
    Source:
    - https://github.com/expressjs/session#options
    - https://github.com/expressjs/session#resave
    - https://github.com/expressjs/session#saveuninitialized

--- config/session.config.js
+++ config/session.config.js
@@ -6,10 +6,13 @@ const sessionName = `ctb-${process.env.SESSION_SECRET ||
   Math.floor(new Date().getTime() / oneHour)}`
 
 // default setup: https://github.com/roccomuso/memorystore#setup
+// options: https://github.com/expressjs/session#options
 module.exports = session({
-  cookie: { maxAge: oneHour },
+  cookie: { httpOnly: true, maxAge: oneHour, sameSite: 'strict' },
   store: new MemoryStore({
     checkPeriod: oneHour, // prune expired entries every hour
   }),
   secret: sessionName,
+  resave: false,
+  saveUninitialized: false,
 })

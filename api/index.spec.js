d3a9f312f1154e87c44e86f8afd7afadf0e51b46
Paul Craig <paul_craig_16@hotmail.com>
2019-10-23 13:51:01 -0400

    Add content changes for the /start page
    Removed reference to the access code, as well as your province
    being from Ontario.

--- api/index.spec.js
+++ api/index.spec.js
@@ -3,15 +3,8 @@ const API = require('./index')
 test('returns expected user with correct login.code', () => {
   const user = API.getUser('A5G98S4K1')
   expect(user).not.toBe(null)
-  expect(user.personal.firstName).toEqual('Gabrielle')
-  expect(user.personal.lastName).toEqual('Roy')
-})
-
-test('returns expected user with correct login.code lowercased', () => {
-  const user = API.getUser('a5g98s4k1')
-  expect(user).not.toBe(null)
-  expect(user.personal.firstName).toEqual('Gabrielle')
-  expect(user.personal.lastName).toEqual('Roy')
+  expect(user.personal.firstName).toBe('[Your name here]')
+  expect(user.personal.lastName).toBe('')
 })
 
 test('returns null with a nonexistent login.code', () => {

d13c2a3cd44e6c601b32f962e18c23efe44dde6e
Kate Drwecka <katedrwecka@gmail.com>
2019-10-24 14:44:46 -0400

    Quickly remove RRSP, Political Contributions, etc Which means I've added them to be skipped in the routes, but not properly deleted the files

--- routes/confirmation/checkAnswersFormat.spec.js
+++ routes/confirmation/checkAnswersFormat.spec.js
@@ -13,49 +13,17 @@ const sessionWithRent = {
   },
 }
 
-const sessionWithPolitical = {
-  ...initialSession,
-  ...{
-    deductions: {
-      politicalContributionClaim: true,
-      politicalFederalAmount: '7.00',
-      politicalProvincialAmount: '10.00',
-    },
-  },
-}
-
 describe('Test checkAnswersFormat function with initialSession', () => { 
   const answerInfo = formatAnswerInfo(initialSession)
   
-  test('it has 3 sections with correct key names', async () => {
-    expect(Object.keys(answerInfo).length).toBe(3)
-    expect(Object.keys(answerInfo)).toEqual(['Personal information', 'Tax claims', 'Tax benefits'])
+  test('it has 2 sections with correct key names', async () => {
+    expect(Object.keys(answerInfo).length).toBe(2)
+    expect(Object.keys(answerInfo)).toEqual(['Personal information', 'Tax benefits'])
   })
 
-  test('it has sections with row lengths of 6, 4, 5', async () => {
+  test('it has sections with row lengths of 6, 5', async () => {
     expect(answerInfo['Personal information'].length).toBe(6)
-    expect(answerInfo['Tax claims'].length).toBe(4)
     expect(answerInfo['Tax benefits'].length).toBe(5)
-  })
-
-  test('it has No in all Tax related rows', async () => {
-    const taxSections = answerInfo['Tax benefits'].concat(answerInfo['Tax claims'])
-    taxSections.map((row) => {
-      expect(row.data).toBe('No')
-    })
-  })
-})
-
-describe('Test checkAnswersFormat with added displayIf rows for Political Contribution', () => {
-  const answerInfo = formatAnswerInfo(sessionWithPolitical)
-  
-  test('it has an extra row in Tax Claims for political contribution amount', async () => {
-    expect(answerInfo['Tax claims'].length).toBe(5)
-  })
-
-  test('it adds the political contribution amount', async () => {
-    const politicalAmount = answerInfo['Tax claims'].find(row => row.urlPath === '/deductions/political/amount')
-    expect(politicalAmount.data).toBe('$17.00')
   })
 })
 

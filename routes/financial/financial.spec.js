bbf1eee0f1eb6b71ed06ae2a822023ae380ac8cd
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2019-11-13 09:56:19 -0700

    Working in financial.spec.js

--- routes/financial/financial.spec.js
+++ routes/financial/financial.spec.js
@@ -1,7 +1,23 @@
 const request = require('supertest')
 const app = require('../../app.js')
+const cheerio = require('cheerio')
 
 describe('Test /financial responses', () => {
+  const session = require('supertest-session')
+
+  function extractCsrfToken(res) {
+    var $ = cheerio.load(res.text);
+    return $('[name=_csrf]').val();
+  }
+  let csrfToken,
+    cookie,
+    testSession = session(app)
+  beforeEach(async () => {
+    const getresp = await testSession.get('/financial/income');
+    if (!cookie)
+      cookie = getresp.headers['set-cookie'];
+    csrfToken = extractCsrfToken(getresp);
+  })
   test('it returns a 200 response for /financial/income', async () => {
     const response = await request(app).get('/financial/income')
     expect(response.statusCode).toBe(200)
@@ -10,14 +26,16 @@ describe('Test /financial responses', () => {
   test('it returns a 422 with no option selected', async () => {
     const response = await request(app)
       .post('/financial/income')
-      .send({ redirect: '/personal/maritalStatus' })
+      .set('Cookie', cookie)
+      .send({ _csrf: csrfToken, redirect: '/personal/maritalStatus' })
     expect(response.statusCode).toBe(422)
   })
 
   test('it returns a 302 and redirects to offramp when NO is selected', async () => {
     const response = await request(app)
       .post('/financial/income')
-      .send({ confirmIncome: 'No', redirect: '/offramp/financial' })
+      .set('Cookie', cookie)
+      .send({ _csrf: csrfToken, confirmIncome: 'No', redirect: '/offramp/financial' })
     expect(response.statusCode).toBe(302)
     expect(response.headers.location).toEqual('/offramp/financial')
   })
@@ -25,7 +43,8 @@ describe('Test /financial responses', () => {
   test('it returns a 302 and redirects to the same page when YES is selected', async () => {
     const response = await request(app)
       .post('/financial/income')
-      .send({ confirmIncome: 'Yes', redirect: '/personal/maritalStatus' })
+      .set('Cookie', cookie)
+      .send({ _csrf: csrfToken, confirmIncome: 'Yes', redirect: '/personal/maritalStatus' })
     expect(response.statusCode).toBe(302)
     expect(response.headers.location).toEqual('/personal/maritalStatus')
   })
@@ -34,7 +53,8 @@ describe('Test /financial responses', () => {
     const response = await request(app)
       .post('/financial/income')
       .query({ref: 'checkAnswers'})
-      .send({ confirmIncome: 'Yes', redirect: '/personal/maritalStatus' })
+      .set('Cookie', cookie)
+      .send({ _csrf: csrfToken, confirmIncome: 'Yes', redirect: '/personal/maritalStatus' })
     expect(response.statusCode).toBe(302)
     expect(response.headers.location).toEqual('/checkAnswers')
   }) 

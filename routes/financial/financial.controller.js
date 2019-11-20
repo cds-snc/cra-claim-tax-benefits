417eb15d7bd5c08021d9d5a5458fa63f1c071138
Paul Craig <paul_craig_16@hotmail.com>
2019-11-20 17:04:07 -0500

    Financial pages come after security questions
    Also put in a bunch of redirect rules so that you don't end up on
    the financial info page when you're not supposed to be.
    
    Also preserve the security question value.

--- routes/financial/financial.controller.js
+++ routes/financial/financial.controller.js
@@ -3,7 +3,25 @@ const { doRedirect, renderWithData, checkErrors } = require('./../../utils')
 const { incomeSchema } = require('./../../schemas')
 
 module.exports = function(app) {
-  app.get('/financial/income', renderWithData('financial/income'))
+  app.get(
+    '/financial/income',
+    (req, res, next) => {
+      // if you have never answered the security question, redir to security Question page
+      if (req.session && req.session.login.securityQuestion === null) {
+        return res.redirect('/login/securityQuestion')
+      }
+      // if you have "skipped" the security question, skip to checkAnswers page
+      // UNLESS you are coming from the checkAsnwers page, in which case, skip to securityQuestion page
+      if (req.session && req.session.login.securityQuestion === '/checkAnswers') {
+        return req.headers.referer.endsWith('/checkAnswers')
+          ? res.redirect('/login/securityQuestion')
+          : res.redirect('/checkAnswers')
+      }
+
+      next()
+    },
+    renderWithData('financial/income'),
+  )
   app.post(
     '/financial/income',
     checkSchema(incomeSchema),

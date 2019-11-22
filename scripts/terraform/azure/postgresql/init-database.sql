ff1de22f6883a327878944479af576063c6181c5
Charles Morin <charles.morin@cds-snc.ca>
2019-11-22 10:34:36 -0500

    added more database objects creation in sql script

--- scripts/terraform/azure/postgresql/init-database.sql
+++ scripts/terraform/azure/postgresql/init-database.sql
@@ -1,16 +1,31 @@
--- Creates a new database table for storing access codes
+CREATE ROLE ctb_user WITH
+	LOGIN
+	NOSUPERUSER
+	NOCREATEDB
+	NOCREATEROLE
+	INHERIT
+	NOREPLICATION
+	CONNECTION LIMIT -1
+	PASSWORD '--> ENTER PASSWORD HERE <--';
+COMMENT ON ROLE tes IS 'Database user for the claim tax benefits front-end application.';
+
 CREATE TABLE public.access_codes
 (
-    code character varying(16) NOT NULL,
-    dob character varying(10) NOT NULL,
+    code character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    dob character varying(10) COLLATE pg_catalog."default" NOT NULL,
     login_count numeric NOT NULL,
-    sin character varying(9) NOT NULL,
+    sin character varying(9) COLLATE pg_catalog."default" NOT NULL,
     locked boolean NOT NULL,
     CONSTRAINT pk_access_code PRIMARY KEY (code)
 )
 WITH (
     OIDS = FALSE
-);
+)
+TABLESPACE pg_default;
 
-ALTER TABLE public.access_codes
-    OWNER to ctb;
+GRANT INSERT, SELECT, UPDATE, DELETE, TRUNCATE, REFERENCES ON TABLE public.access_codes TO ctb_user;
+
+GRANT ALL ON TABLE public.access_codes TO ctb_admin;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT INSERT, SELECT, UPDATE, DELETE, TRUNCATE, REFERENCES ON TABLES TO ctb_user;

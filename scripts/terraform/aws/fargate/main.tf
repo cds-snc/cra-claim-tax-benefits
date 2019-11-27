a17c4965bdf221f0f0b71bdefcd66e4c1f6b4628
Charles Morin <charles.morin@cds-snc.ca>
2019-11-27 08:51:31 -0500

    segregation of steps into two different folders

--- scripts/terraform/aws/fargate/main.tf
+++ scripts/terraform/aws/fargate/main.tf
@@ -1,4 +1,5 @@
 terraform {
+  # Defines a backend (S3 bucket) where to store the Terraform state. 
   backend "s3" {
       bucket = "ctb-terraform-state-bucket"
       key = "terraform.tfstate"
--- scripts/terraform/aws/fargate/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "terraform" {
-  bucket = "ctb-terraform-state-bucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "Dev"
-  }
-}

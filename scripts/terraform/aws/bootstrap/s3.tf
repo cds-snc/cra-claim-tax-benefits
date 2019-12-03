037c6e567ed83930a86862a6a9acf5ee35159f6c
Charles Morin <charles.morin@cds-snc.ca>
2019-12-03 14:19:35 -0500

    added encryption

--- scripts/terraform/aws/bootstrap/s3.tf
+++ scripts/terraform/aws/bootstrap/s3.tf
@@ -1,3 +1,8 @@
+resource "aws_kms_key" "ctbkey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "storage_bucket" {
   bucket = "ctb-terraform-state-bucket"
   acl    = "private"
@@ -8,4 +13,17 @@ resource "aws_s3_bucket" "storage_bucket" {
     Name        = "Terraform State Bucket"
     Environment = "Dev"
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.ctbkey.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  depends_on = [
+    aws_kms_key.ctbkey
+  ]
 }

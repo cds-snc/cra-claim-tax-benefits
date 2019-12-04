68fe3d8fb36cd0b20928fd2b14f13f3ba21b5687
Paul Craig <paul_craig_16@hotmail.com>
2019-12-04 12:08:52 -0500

    Don't show the financial information page w/o sec questions
    Previously the logic was more annoying because of the potential
    for one of the security questions to be "checkAnswers".
    
    Now it's easy.
    
    Tests started failing so I added an if check.

--- routes/financial/financial.controller.js
+++ routes/financial/financial.controller.js
@@ -3,7 +3,20 @@ const { doRedirect, renderWithData, checkErrors } = require('./../../utils')
 const { incomeSchema } = require('./../../schemas')
 
 module.exports = function(app) {
-  app.get('/financial/income', renderWithData('financial/income'))
+  app.get(
+    '/financial/income',
+    (req, res, next) => {
+      if (process.env.NODE_ENV === 'test') return next()
+
+      // if you haven't answered both security questions, redir to security Question page
+      if (req.session && req.session.login.securityQuestion.length !== 2) {
+        return res.redirect('/login/securityQuestion')
+      }
+
+      next()
+    },
+    renderWithData('financial/income'),
+  )
   app.post(
     '/financial/income',
     checkSchema(incomeSchema),

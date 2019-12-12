2477dd2eaaee0db59493963a11671c3b34361a04
nmakuch <makuch.nick@gmail.com>
2019-12-11 22:23:57 -0500

    logic to check age conditional for transit credit

--- routes/personal/personal.controller.js
+++ routes/personal/personal.controller.js
@@ -1,5 +1,5 @@
 const { checkSchema } = require('express-validator')
-const { doRedirect, renderWithData, checkErrors } = require('./../../utils')
+const { doRedirect, renderWithData, checkErrors, getDateDelta } = require('./../../utils')
 const {
   addressSchema,
   maritalStatusSchema,
@@ -44,7 +44,6 @@ module.exports = function(app) {
   )
 }
 
-
 const postResidence = (req, res, next) => {
   if (req.body.residence !== 'Ontario') {
     return res.redirect('/offramp/residence')
@@ -55,9 +54,9 @@ const postResidence = (req, res, next) => {
 
 const postName = (req, res, next) => {
   const name = req.body.name
-  
+
   req.session.personal.confirmedName = name
-  
+
   if (name !== 'Yes') {
     return res.redirect('/offramp/name')
   }
@@ -67,7 +66,7 @@ const postName = (req, res, next) => {
 
 const postConfirmMaritalStatus = (req, res, next) => {
   const confirmMaritalStatus = req.body.confirmMaritalStatus
-  
+
   req.session.personal.confirmedMaritalStatus = confirmMaritalStatus
 
   if (confirmMaritalStatus === 'No') {
@@ -76,12 +75,17 @@ const postConfirmMaritalStatus = (req, res, next) => {
     return res.redirect('/offramp/maritalStatus')
   }
 
+  // Just basing the difference in months or years is not a good enough check so we are checking the total delta days
+  if (getDateDelta(req.session.personal.dateOfBirth) === 23725 && confirmMaritalStatus === 'Yes') {
+    return res.redirect('/deductions/senior-public-transit')
+  }
+
   next()
 }
 
 const postConfirmAddress = (req, res, next) => {
   const confirmAddress = req.body.confirmAddress
-  
+
   req.session.personal.confirmedAddress = confirmAddress
 
   if (confirmAddress === 'No') {

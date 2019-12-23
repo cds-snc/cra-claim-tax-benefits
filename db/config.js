4ee319bc721ef445d8104782f06222f08c565c4f
Kate Drwecka <katedrwecka@gmail.com>
2019-12-23 14:25:02 -0500

    remove isProduction check since it returns the same thing, and it annoys SonarCloud right now

--- db/config.js
+++ db/config.js
@@ -1,14 +1,13 @@
 require('dotenv').config()
 
 const { Pool } = require('pg')
-const isProduction = process.env.NODE_ENV === 'production'
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 
-// TODO: swap in needed azure creds here. Right now it's the same thing either way, but just have it set up to look for production
+// TODO: bring back isProduction check and swap in needed azure creds here. Right now it's the same thing either way, but just have it set up to look for production
 const pool = new Pool({
-  connectionString: isProduction ? connectionString : connectionString,
+  connectionString: connectionString,
   ssl: isProduction,
 })
 

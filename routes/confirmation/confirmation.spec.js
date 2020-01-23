cb56ec4a3b030a89974c7bb52d051bfe3b1676b0
Kate Drwecka <katedrwecka@gmail.com>
2020-01-23 16:15:45 -0500

    remove review page and all associated files this includes schema, routes, test, and unused lang keys now

--- routes/confirmation/confirmation.spec.js
+++ routes/confirmation/confirmation.spec.js
@@ -3,7 +3,7 @@ const { extractCsrfToken, withCSRF } = require('../utils.spec')
 const app = require('../../app.js')
 
 describe('Test confirmation urls', () => {
-  const urls = ['/review', '/checkAnswers', '/confirmation', '/feedback']
+  const urls = ['/checkAnswers', '/confirmation', '/feedback']
 
   urls.map(url => {
     test(`${url} returns a 200 response`, async () => {
@@ -18,61 +18,6 @@ describe('Test /download', () => {
     const response = await request(app).get('/download')
     expect(response.statusCode).toBe(500)
     expect(response.text).toMatch('Download not available')
-  })
-})
-
-describe('Test /review', () => {
-  const session = require('supertest-session')
-
-  let csrfToken, cookie
-
-  beforeEach(async () => {
-    let testSession = session(app)
-    const getresp = await testSession.get('/login/code')
-    cookie = getresp.headers['set-cookie']
-    csrfToken = extractCsrfToken(getresp)
-  })
-
-  describe('POST responses', () => {
-    test('it returns a 422 response if no values are posted', async () => {
-      const response = await request(app)
-        .post('/review')
-        .use(withCSRF(cookie, csrfToken))
-      expect(response.statusCode).toBe(422)
-    })
-
-    test('it returns a 422 response for no posted value', async () => {
-      const response = await request(app)
-        .post('/review')
-        .use(withCSRF(cookie, csrfToken))
-        .send({ redirect: '/confirmation' })
-      expect(response.statusCode).toBe(422)
-    })
-
-    test('it returns a 500 response if no redirect is provided', async () => {
-      const response = await request(app)
-        .post('/review')
-        .use(withCSRF(cookie, csrfToken))
-        .send({ review: 'review' })
-      expect(response.statusCode).toBe(500)
-    })
-
-    test('it returns a 422 response for the wrong value', async () => {
-      const response = await request(app)
-        .post('/review')
-        .use(withCSRF(cookie, csrfToken))
-        .send({ review: 'get er done', redirect: '/confirmation' })
-      expect(response.statusCode).toBe(422)
-    })
-
-    test('it returns a 302 response for the right value', async () => {
-      const response = await request(app)
-        .post('/review')
-        .use(withCSRF(cookie, csrfToken))
-        .send({ review: 'review', redirect: '/confirmation' })
-      expect(response.statusCode).toBe(302)
-      expect(response.headers.location).toEqual('/confirmation')
-    })
   })
 })
 
--- schemas/index.js
+++ schemas/index.js
@@ -1,11 +1,9 @@
-const confirmationSchemas = require('./confirmation.schema')
 const deductionsSchemas = require('./deductions.schema')
 const loginSchemas = require('./login.schema')
 const personalSchemas = require('./personal.schema')
 const votingSchemas = require('./vote.schema')
 
 module.exports = {
-  ...confirmationSchemas,
   ...deductionsSchemas,
   ...loginSchemas,
   ...personalSchemas,

137ed507817a4cc3376d75547fe4a541d6f50424
nmakuch <makuch.nick@gmail.com>
2020-02-24 15:54:11 -0500

    passing in path as back url

--- routes/cancel/cancel.controller.js
+++ routes/cancel/cancel.controller.js
@@ -1,8 +1,6 @@
 module.exports = function(app) {
   app.get('/cancel', (req, res) => {
-    console.log(req.query)
-    const query = { back: '/eligibility/age' }
-    const back = query.back
+    const back = req.query.back
     return res.render('cancel/cancel', {
       data: req.session,
       back,
